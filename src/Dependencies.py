c2a0fbcf90b2659ac44b7cb7c40ff0f14cd672f6
SupakornMhee <6422780146@g.siit.tu.ac.th>
2024-11-06 13:39:56 +0700

    Mhee ปรับ main.py นิดหน่อย

--- src/Dependencies.py
+++ src/Dependencies.py
@@ -3,7 +3,7 @@ from src.Util import SpriteManager
 import src.Util as Util
 
 from src.StateMachine import StateMachine
-# from src.states.game.PlayState import PlayState
+from src.states.game.PlayState import PlayState
 from src.states.game.StartState import StartState
 from src.states.game.StoryState import StoryState
 from src.states.game.LastVictoryState import LastVictoryState
