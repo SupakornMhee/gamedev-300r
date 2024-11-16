0622a6f502459503414f6e235d9e0bd82372290f
SupakornMhee <6422780146@g.siit.tu.ac.th>
2024-11-16 20:59:01 +0700

    Merge commit '7597da42f32fc04df9cbfb5520b0f755393de560'

--- src/recourses.py
+++ src/recourses.py
@@ -26,3 +26,13 @@ gGeeGee_animation_list = {"right": sprite_collection["geegee_walk_right"].animat
                         "attack_right": sprite_collection["geegee_attack_right"].animation,
                         "attack_left": sprite_collection["geegee_attack_left"].animation
 }
+
+gXerxes_animation_list = {"walk": sprite_collection["xerxes_walk"].animation,
+                        "attack_right": sprite_collection["loog_nong_attack_right"].animation,
+                        "attack_left": sprite_collection["loog_nong_attack_left"].animation
+}
+gLoog_nong_animation_list = {"right": sprite_collection["loog_nong_walk"].animation,
+                         "left": sprite_collection["loog_nong_walk"].animation,
+                        "attack_right": sprite_collection["loog_nong_attack_right"].animation,
+                        "attack_left": sprite_collection["loog_nong_attack_left"].animation
+}
